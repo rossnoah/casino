7064e59580d58a9cfab4983dcb5adb4b0b885fd7
Noah Ross <me@noah.dev>
2024-09-10 22:22:06 -0400

    add tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,11 @@ export default function RootLayout({
         <title>Casino Sim</title>
         <meta name="description" content="Play some fake games of chance!" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <script
+          defer
+          src="https://umami-qs0c004ww04o0c0o8woocwws.noah.dev/script.js"
+          data-website-id="7f7430b7-17c0-4c28-be0c-af0a6fac4fd3"
+        ></script>
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
